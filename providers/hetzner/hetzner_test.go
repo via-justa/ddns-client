17e4151f77adc14406b0dcbf41595d1799a7704c
via-justa <25720282+via-justa@users.noreply.github.com>
2020-07-24 23:48:28 +0200

    Fix TestNewDNSProvider

--- providers/hetzner/hetzner_test.go
+++ providers/hetzner/hetzner_test.go
@@ -2,15 +2,25 @@
 package hetzner
 
 import (
+	"os"
 	"testing"
 )
 
+var testEnvVal = "test"
+
 func TestNewDNSProvider(t *testing.T) {
 	t.Run("working config", func(t *testing.T) {
-		_, err := NewDNSProvider()
+		preTestEnvVal := os.Getenv(envAPIKey)
+		os.Setenv(envAPIKey, testEnvVal)
+		got, err := NewDNSProvider()
 		if err != nil {
 			t.Errorf("NewDNSProvider() error = %v", err)
 			return
 		}
+		if got.config.APIKey != testEnvVal {
+			t.Errorf("NewDNSProvider() error = Env key not match")
+			return
+		}
+		os.Setenv(envAPIKey, preTestEnvVal)
 	})
 }
